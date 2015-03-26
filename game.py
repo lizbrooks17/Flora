b0f1938c2148adfb64ad783c8f706a0c8ecc3f8b
Liz <lizbrooks17@yahoo.com>
2015-03-25 22:46:32 -0400

    added fog-of-war

--- game.py
+++ game.py
@@ -5,45 +5,73 @@
 
 import sys
 import pygame
-from terrain import Terrain
+from terrain import Terrain, colors
 
 pygame.init()
-size = width, height = 700, 500
+board_size = width, height = 718, 480
+tile_size = 24
+visibility = 1
 
-screen = pygame.display.set_mode(size)
-screen.fill((192,192,192))   # light gray
+screen = pygame.display.set_mode(board_size)
+screen.fill(colors['light gray'])
 
-t = Terrain(width, height)
+t = Terrain(width, height, tile_size)
 for row in t.terrain:
     for tile in row:
         screen.blit(tile.get_surface(),tile.get_position())
 pygame.display.flip()
+sqY = len(t.terrain)
+sqX = len(t.terrain[0])
 
-me = pygame.Surface((20,20))
+me = pygame.Surface((tile_size,tile_size))
 me.fill((0,0,0))
-me.fill((255,255,51),(5,5,10,10))   #yellow
+me.fill(colors['yellow'],(tile_size/4,tile_size/4,tile_size/2,tile_size/2))
 me.set_colorkey((0,0,0))
 
-def coords(i,j):
-    return (i*20,j*20)
+fog = pygame.Surface((tile_size,tile_size))
+fog.fill(colors['black'])
+fog.set_alpha(125)
 
-myX = width/20/2
-myY = height/20/2
+def coords(i,j):
+    return (i*tile_size,j*tile_size)
+
+def visible(X,Y,V):
+    current = []
+    for x in range(X-V, X+V+1):
+        for y in range(Y-V, Y+V+1):
+            if x<0 or x>=sqX: continue
+            if y<0 or y>=sqY: continue
+            t.terrain[y][x].make_visible()
+            current.append(t.terrain[y][x])
+    return current
+
+myX = width/tile_size/2
+myY = height/tile_size/2
+current = visible(myX,myY,visibility)
+for tile in current:
+    screen.blit(tile.get_surface(),tile.get_position()) # surrounding tiles
 screen.blit(me, coords(myX,myY))
 pygame.display.flip()
+
 
 while 1:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
         elif event.type == pygame.MOUSEBUTTONDOWN:
-            tile = t.terrain[myY][myX]
-            screen.blit(tile.get_surface(),tile.get_position()) # blit background
-            cuX,cuY = pygame.mouse.get_pos()
-            if cuX/20 < myX: myX = myX - 1
-            if cuX/20 > myX: myX = myX + 1
-            if cuY/20 < myY: myY = myY - 1
-            if cuY/20 > myY: myY = myY + 1
+            (cuX,cuY) = pygame.mouse.get_pos()
+            for tile in current:
+                surf = tile.get_surface()
+                pos = tile.get_position()
+                screen.blit(surf,pos) # blit background
+                screen.blit(fog,pos)
+            if cuX/tile_size < myX: myX = myX - 1
+            if cuX/tile_size > myX: myX = myX + 1
+            if cuY/tile_size < myY: myY = myY - 1
+            if cuY/tile_size > myY: myY = myY + 1
+            current = visible(myX,myY,visibility)
+            for tile in current:
+                screen.blit(tile.get_surface(),tile.get_position()) # surrounding tiles
             screen.blit(me, coords(myX,myY))  # blit me
             pygame.display.flip()
 
@@ -51,3 +79,4 @@ while 1:
 #sys.exit()
 #    elif event.type == pygame.MOUSEBUTTONDOWN: greencube.fill(white)
 #    elif event.type == pygame.MOUSEBUTTONUP: greencube.fill(green)
+
--- terrain.py
+++ terrain.py
@@ -6,17 +6,22 @@
 import pygame
 import random
 
-all_land = { 'black':(0,0,0),
-             'red':(255,0,0),
-             'blue':(0,0,255),
-             'green':(0,255,0),
-             'white':(255,255,255), }
+NO = 0
+YES = 1
+colors = { 'black':(0,0,0),
+           'red':(255,0,0),
+           'blue':(0,0,255),
+           'green':(0,255,0),
+           'white':(255,255,255),
+           'yellow':(255,255,51),
+           'light gray':(192,192,192),
+          }
 
 blues = [(102,178,255),(0,128,255),(0,128,255),(0,0,255),(0,0,255),(0,0,153),]
 
 def set_color():
-    #color = random.choice(all_land.keys())
-    #return all_land[color]
+    #color = random.choice(colors.keys())
+    #return colors[color]
     color = random.choice(blues)
     return color
 
@@ -24,34 +29,43 @@ def set_flower():
     return None
 
 class Tile:
-    def __init__(self,X,Y):
+    def __init__(self,X,Y,side):
         self.X = X
         self.Y = Y
-        self.surface = pygame.Surface((20,20))
-        #self.color = set_color()
-        self.surface.fill(set_color())
+        self.side = side
+        self.surface = pygame.Surface((self.side,self.side))
+        self.surface.fill(colors['black'])
+        self.color = set_color()
+        self.visible = NO
         self.flower = set_flower()
     def get_position(self):
-        return (self.X,self.Y)
+        x = self.X * self.side
+        y = self.Y * self.side
+        return (x,y)
     def get_surface(self):
-        #self.surface.fill(self.color)
+        if self.visible:
+            self.surface.fill(self.color)
         return self.surface
+    def make_visible(self):
+        self.visible = YES
 
 class Terrain:
     terrain = []
-    def __init__(self,X,Y):
-        for j in range(Y/20):
+    def __init__(self,width,height,side):
+        for j in range(height/side):
             alist = []
-            for i in range(X/20):
-                alist.append( Tile(i*20,j*20) )
+            for i in range(width/side):
+                alist.append( Tile(i,j,side) )
             self.terrain.append(alist)
 
 
 if __name__ == '__main__':
-    board = Terrain(105,49)
+    board = Terrain(105,49,24)
     for row in board.terrain:
         for tile in row:
             print tile.get_position(),
         print
-
+        for tile in row:
+            print str(tile.X) +','+ str(tile.Y),
+        print
 
